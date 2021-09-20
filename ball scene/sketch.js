6ebcac921220b987e32edc0427034734504745ea
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-09-20 10:16:54 -0600

    made ball sn

--- ball scene/sketch.js
+++ ball scene/sketch.js
@@ -1,20 +1,56 @@
-// Project Title
+// ball scene
 // Your Name
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let ballArray =[];
+// let x, y, dx, dy, radius, ballColour; 
 
-let carlsLeftBootyCheeksStinky = 100;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+
+  for (let i = 0; i < 100000; i ++){
+    let newBall = {
+      x: random(width),
+      y: random(height),
+      radius: random(10, 30),
+      ballColour: color(random(255), random(255), random(255), random(255)),
+      dx: random(5,10),
+      dy: random(5,10)
+    };
+    ballArray.push(newBall);
+  }
 }
 
 function draw() {
-  backgdound('aqua');
-  rect(mouseX,mouseY,carlsLeftBootyCheeksStinky,100);
+  background('lightblue');
+  displayBall();
+  moveBall();
 }
+
+function moveBall() {
+  for (let theBall of ballArray) {
+    theBall.x+= theBall.dx; 
+    theBall.y+= theBall.dy;
+    theBall.dx = random(-5,5);
+    theBall.dy = random(-5,5);
+}
+
+}
+
+
+function displayBall() {
+  for (let ball of ballArray){
+    noStroke();
+    fill(ball.ballColour);
+    circle(ball.x,ball.y,ball.radius*2);
+    
+  }
+}
+
 
