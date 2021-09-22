e681137767a3375a991bfc02fb6a40c910469899
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-09-22 11:01:54 -0600

    sun

--- hills/sketch.js
+++ hills/sketch.js
@@ -1,13 +1,40 @@
 // terrain
 
 
+let heights = [];
+let numberOfRect;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  numberOfRect = width;
+  genTer(numberOfRect);
 }
 
 function draw() {
-  background(220);
+  background('lightblue');
+  fill('yellow')
+  ellipse(100, 100, 100, 100)
+  displayTerrain();
 }
 
+function displayTerrain(){
+  let rectWidth = width/numberOfRect;
+  fill('green');
+  noStroke();
+  for(let i=0; i <heights.length; i++){
+    rect(i*rectWidth,height-heights[i],rectWidth,heights[i]);
+  }  
+}
+
+function genTer(howMany){
+  let time = 0;
+  let dTime= 0.001;
+
+  for (let i = 0; i<howMany; i++){
+    let currentHeight = noise(time)*height;
+    heights.push(currentHeight);
+    time+=dTime;
+
+  }
+}
