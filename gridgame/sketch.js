37186ce523c00e4333b4a7badc16d9adcd54a91b
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-10-04 08:54:18 -0600

    woah

--- gridgame/sketch.js
+++ gridgame/sketch.js
@@ -30,12 +30,12 @@ function displayGrid(){
   let cellSize = width/gridSize;
   for (let y=0; y <grid.length; y++){
     for(let x=0;x<grid[y].length; x++){
-      // if(grid[y][x] === 0){
-      //   fill("white");
-      // } 
-      // else if(grid[x][y] === 1){
-      //   fill("black");
-      // }
+      if(grid[y][x] === 0){
+        fill("white");
+      } 
+      else if(grid[x][y] === 1){
+        fill("black");
+      }
       rect(x*cellSize,y*cellSize,cellSize,cellSize);
     }
   }
