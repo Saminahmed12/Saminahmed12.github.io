af9a6cdb953d0a559074a130905f760a53b24cf9
Saminahmed12 <90922359+Saminahmed12@users.noreply.github.com>
2021-10-18 09:38:20 -0600

    sounds

--- gridbasedgame/sketch.js
+++ gridbasedgame/sketch.js
@@ -1,23 +1,27 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// whack a mole with a bad twist
+// Samin Ahmed
+// 10/18/2021
+
+
+// globalVariables
 let gridDim = 5;
 let grid;
 let sqrSize;
 let mole;
 let timer = 4;
 let grass;
-
-
+let theme;
+let squish;
 
 function preload(){
+  // loadsSoundsAndImages
+  theme = loadSound("assets/theme.mp3");
+  squish = loadSound("assets/squish.mp3");
   mole = loadImage("assets/mole.png");
   grass = loadImage("assets/grass.jpg");
 }
 function setup() { 
+  // setsUpCanvasAndArray
   if (windowHeight > windowWidth){
     createCanvas(windowWidth, windowWidth);
   }
@@ -30,18 +34,21 @@ function setup() {
 }
 
 function draw() {
+  // drawsEverything
   background(grass);
   displayGrid();
   gameTimer();
-  
+  if (keyIsDown(32)){
+    theme.loop();
+  }
 }
 
 function mousePressed() {
+  // swapsTilesWhenMouseClicked
   if (timer > 0){
     if (mouseX <= width && mouseY <= height) {
       let sqrX = Math.floor(mouseX/sqrSize);
       let sqrY = Math.floor(mouseY/sqrSize);
-      
       swap(sqrX, sqrY);
     }
   }
@@ -49,18 +56,21 @@ function mousePressed() {
   
 
 function swap(x, y) {
+  // swaps0To1AndViseVersa
   if (x >= 0 && x < gridDim && y >= 0 && y < gridDim) {
     if (grid[y][x] === 0) {
       grid[y][x] = 1;
     }
     else if (grid[y][x] === 1) {
       grid[y][x] = 0;
+      squish.play();
     }
   }
 }
 
 
 function displayGrid() {
+  // displaysTheGrid
   for (let y=0; y<gridDim; y++) {
     for (let x=0; x<gridDim; x++) {
       if(grid[y][x] === 0) {
@@ -78,6 +88,7 @@ function displayGrid() {
 
 
 function createRandomArray(howLarge){
+  // createsNewAraay
   let newArray = [];
   for (let y = 0; y < howLarge; y++){
     newArray.push([]);
@@ -95,13 +106,13 @@ function createRandomArray(howLarge){
 }
 
 function gameTimer() {
+  // timerForTheGame
   textAlign(CENTER, CENTER);
   textSize(100);
   fill("black");
   if (timer > 0){
     text(timer, width/2, height/2);
   }
-  
   if (frameCount % 60 === 0 && timer > 0) { 
     timer --;
   }
@@ -118,12 +129,12 @@ function gameTimer() {
 
 
 function didYouWin(){
+  // checkingIfYouWon
   let counter =0;
   for (let y=0; y<gridDim; y++){
     for (let x=0; x<gridDim; x++){
       if (grid[y][x] === 1){
         counter++;
-        
       }  
     }
   }
